1c424c73d1bea1d31c637c1ea1de138831e1e35c
tomabou <kazu.8128.9.6@gmail.com>
2018-10-01 22:26:51 +0900

    read label

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 
 fn main(){
     println!("Hello World");
-    read_file();
+    read_test_label();
+    read_train_label();
 }
 
 use std::{fs, slice};
 use std::io::{BufReader, Read};
 use std::path::Path;
 
-fn read_file() {
-    let path = Path::new("../data/t10k-labels-idx1-ubyte");
-    let mut f = BufReader::new(fs::File::open(&path).unwrap());
-    let mut m: i32 = 0;
-    let buf = unsafe{slice::from_raw_parts_mut(&mut m as *mut i32 as *mut u8, 4) };
+fn read_label_file(path: &std::path::Path) {
+    let mut f = BufReader::new(fs::File::open(path).unwrap());
+    let mut n: i32 = 0;
+    let buf = unsafe{slice::from_raw_parts_mut(&mut n as *mut i32 as *mut u8, 4) };
     f.read_exact(buf).unwrap();
-    m = i32::from_be(m);
+    f.read_exact(buf).unwrap();
+    n = i32::from_be(n);
     
-    println!("{}",m);
+    println!("number of lable: {}",n);
+}
+
+fn read_test_label(){
+    let path = Path::new("./data/t10k-labels-idx1-ubyte");
+    read_label_file(path)
+}
+fn read_train_label(){
+    let path = Path::new("./data/train-labels-idx1-ubyte");
+    read_label_file(path)
 }
