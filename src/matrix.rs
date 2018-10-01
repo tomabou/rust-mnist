38408a431c8fe5ea5b5e40fdc303d26a05c1d215
tomabou <kazu.8128.9.6@gmail.com>
2018-10-02 00:32:28 +0900

    matmul

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,20 +1,26 @@
 use std::ops::Add;
+use std::ops::AddAssign;
 
 #[derive(Debug,PartialEq)]
 pub struct Matrix{
-    shape: (i32,i32),
+    shape: (usize,usize),
+    val: Vec<f32>
+}
+
+#[derive(Debug,PartialEq)]
+pub struct Vector{
     val: Vec<f32>
 }
 
 impl Matrix{
-    pub fn add(&mut self,m: &Matrix) -> &Matrix{
+    fn add(&mut self,m: &Matrix){
         assert_eq!(self.shape,m.shape);
-        for i in 0..(self.shape.0*self.shape.1) as usize {
+        for i in 0..self.shape.0*self.shape.1 {
             self.val[i] += m.val[i];
         }
-        self
     }
 }
+
 
 #[test]
 fn test_add(){
@@ -25,4 +31,21 @@ fn test_add(){
     m.add(&n);
     let ans = Matrix{shape:(2,2),val:vec![2.0,4.0,6.0,8.0]};
     assert_eq!(m,ans);
+}
+
+pub fn mat_mul(m: &Matrix, v: &Vector) -> Vector{
+    assert!(m.shape.1 == v.val.len());
+    let mut res = Vector{val: vec![0.0;m.shape.0]};
+    for i in 0..m.shape.0{
+        for j in 0..m.shape.1{
+            res.val[i] += m.val[i*m.shape.1+j] * v.val[j];
+        }
+    }
+    res
+}
+#[test]
+fn test_mut_mul(){
+    let x = Matrix{shape:(2,2),val:vec![1.0,2.0,3.0,4.0]};
+    let y = Vector{val:vec![1.0,2.0]};
+    assert_eq!(mat_mul(&x, &y),Vector{val:vec![5.0,11.0]});
 }
